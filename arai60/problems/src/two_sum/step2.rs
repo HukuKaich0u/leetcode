3251a6197e08fb097ac61cbaa63a314a111c0d85
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-02-14 21:51:11 +0900

    fix(typo)

--- arai60/problems/src/two_sum/step2.rs
+++ arai60/problems/src/two_sum/step2.rs
@@ -47,7 +47,7 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         for i in 0..nums.len() {
             // iとjが異なるという条件を表現している
-            for j in (j+1)..nums.len() {
+            for j in (i+1)..nums.len() {
 
                 if nums[i] + nums[j] == target {
                     return vec![i as i32, j as i32];
